403fab608603da57c77b894548245a85e1a4ce30
paulzambelli <paulzambelli@student.ethz.ch>
2026-03-11 16:41:59 +0100

    Initial commit.

---
+++ setup.bash
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Modify this for your environment
+
+if [[ -z "${ARDUPILOT_GAZEBO}" ]]; then
+  export ARDUPILOT_GAZEBO="$HOME/ardupilot_gazebo"
+fi
+
+if [[ -z "${ARDUPILOT_HOME}" ]]; then
+  export ARDUPILOT_HOME="$HOME/ardupilot"
+fi
+
+if [[ -z "${COLCON_WS}" ]]; then
+  export COLCON_WS="$HOME/colcon_ws"
+fi
+
+
+# Add results of ArduSub build
+export PATH=${ARDUPILOT_HOME}/build/sitl/bin:$PATH
+
+# Add results of colcon build
+source ${COLCON_WS}/install/setup.bash
+
+# This is added by ardupilot_gazebo:
+# export GZ_SIM_SYSTEM_PLUGIN_PATH=${ARDUPILOT_GAZEBO}/build:$GZ_SIM_SYSTEM_PLUGIN_PATH
+
+# This is added by bluerov2_gz:
+# export GZ_SIM_RESOURCE_PATH=${COLCON_WS}/src/bluerov2_gz/models:${COLCON_WS}/src/bluerov2_gz/worlds:$GZ_SIM_RESOURCE_PATH
+
+# This is added by orca4/orca_description:
+# export GZ_SIM_RESOURCE_PATH=${COLCON_WS}/src/orca4/orca_description/models:${COLCON_WS}/src/orca4/orca_description/worlds:$GZ_SIM_RESOURCE_PATH
+
+# Build ros_gz on the humble branch for Gazebo Harmonic
+export GZ_VERSION=harmonic
